790ad756ff89e4e50927956883f0e79ac1dd12ee
Derek <derek.yadlowski@sait.ca>
2025-10-22 10:09:45 -0600

    Part 1 Examples

--- App.jsx
+++ App.jsx
@@ -6,11 +6,12 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import MyFirstComponent from "./src/components/my-component.jsx";
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -26,12 +27,12 @@ function App() {
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
+  console.log("Hello World!");
+
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={ {fontSize:30,color:'blue',textAlign:'center'} }>Hello World!</Text>
+      <MyFirstComponent />
     </View>
   );
 }
@@ -39,6 +40,8 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 20
   },
 });
 
---
+++ src/components/my-component.jsx
@@ -0,0 +1,55 @@
+import { Button, Image, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+
+
+export default function MyFirstComponent(){
+
+    function sayHello(){
+        alert('Hello World!');
+    }
+
+    return(
+        <View>
+            <Text style={myStyles.myText}>This is a React-Native component</Text>
+            <Image style={myStyles.myImage} source={ require('../../assets/images/cat.jpg') } />
+            <Image style={myStyles.myImage} source={ {uri: 'https://images.dog.ceo/breeds/kelpie/n02105412_7513.jpg'} } />
+            <Button title="Say Hello!" onPress={sayHello} />
+            <Pressable style={myStyles.myButton} onPress={sayHello}>
+                <Text style={myStyles.myButtonText}>Say Hello</Text>
+            </Pressable>
+            <View>
+                <Text>Simple Form</Text>
+                <Text>Name:</Text>
+                <TextInput style={myStyles.myTextInput} placeholder="John Smith" />
+                <Text>Phone:</Text>
+                <TextInput style={myStyles.myTextInput} keyboardType="numeric" />
+                <Text>Password:</Text>
+                <TextInput style={myStyles.myTextInput} secureTextEntry />
+            </View>
+        </View>
+    );
+}
+
+const myStyles = StyleSheet.create({
+    myText: {
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    myImage: {
+        width: 320,
+        height: 200
+    },
+    myButton:{
+        backgroundColor: 'green',
+        padding: 10,
+    },
+    myButtonText: {
+        fontSize:20,
+        color: 'white',
+        textAlign: 'center'
+    },
+    myTextInput: {
+        borderWidth: 2,
+        borderColor: 'blue'
+    }
+
+});
